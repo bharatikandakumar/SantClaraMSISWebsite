8c0323c569fa95945449cee0fee6568fafba8eba
Bharati Pradayini Kandakumar <45185206+bharatikandakumar@users.noreply.github.com>
2020-03-26 16:39:09 -0700

    Updates

---
+++ precache-manifest.1ddb91833b792481ce5b59915de39307.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b8fd8d6a9e92d422eb0e89d7299b18c",
+    "url": "/SantClaraMSISWebsite/index.html"
+  },
+  {
+    "revision": "a3c47100e93d69e8a7f3",
+    "url": "/SantClaraMSISWebsite/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "137d47b51fa9cc8c8d68",
+    "url": "/SantClaraMSISWebsite/static/js/2.cf976701.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SantClaraMSISWebsite/static/js/2.cf976701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3c47100e93d69e8a7f3",
+    "url": "/SantClaraMSISWebsite/static/js/main.b8ff8ab3.chunk.js"
+  },
+  {
+    "revision": "88c15aab1191da990277",
+    "url": "/SantClaraMSISWebsite/static/js/runtime-main.1cb4081b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SantClaraMSISWebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
